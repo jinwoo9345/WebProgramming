40aa4d74ff469b0d10190c5933d21cb2fb3cc2bb
jinwoo9345 <jjw9345@gmail.com>
2024-12-08 16:22:00 +0900

    Update funcPr-practice.js

--- funcPr-practice.js
+++ funcPr-practice.js
@@ -1,29 +1,40 @@
 const cart = [
-    { item: "노트북", price: 1200000, quantity: 1 }, 
-    { item: "마우스", price: 35000, quantity: 2 },
-    { item: "키보드", price: 89000, quantity: 1 } 
-   ];
-   
-   let totalPrice = 0;
-   for (let i=0; i<cart.length; i++)
-   {
-    totalPrice+= cart[i].price*cart[i].quantity;
-   }
-   console.log(`Total Price: ${totalPrice}`);
-   
-   totalPrice = 0;
-   cart.forEach(good=>totalPrice+=good.price+good.quantity);
-   console.log(`Total Price: ${totalPrice}`);
-   
-   
-   totalPrice = cart.reduce((a,goods)=>(a+goods.price*goods.quantity),0);
-   console.log(`Total Price: ${totalPrice}`);
- 
-   const itemTotal = cart.map(goods =>({
+    { item: "laptop", price: 1200000, quantity: 1 },
+    { item: "mouse", price: 35000, quantity: 2 },
+    { item: "keyboard", price: 89000, quantity: 1 },
+];
+let totalPrice = 0;
+
+for (let i = 0; i < cart.length; i++) {
+    totalPrice += cart[i].price * cart[i].quantity;
+}
+console.log(`Total price: ${totalPrice}`);
+
+totalPrice = 0;
+
+cart.forEach(goods => totalPrice += goods.price * goods.quantity);  
+console.log(`Total price: ${totalPrice}`);
+
+
+totalPrice = cart.reduce((res, goods) => (res + goods.price * goods.quantity), 0);
+console.log(`Total price: ${totalPrice}`);
+
+
+const itemTotals = cart.map(goods => ({
     item: goods.item,
-    total: goods.price*goods.quantity
-   }));
+    total: goods.price * goods.quantity
+}
+));
+console.log('price total:',itemTotals);
 
-   console.log('제품별 금액:' ,itemTotal);
+const names=['alice','bob','charlie'];
+const upperCaseNames=names.map(
+    names=> names.toUpperCase()
+);
+console.log(upperCaseNames);
 
- 
+const capitalStartNames=names.map(
+    
+    names=>names.charAt(0).toUpperCase()+names.slice(1)
+)
+console.log(capitalStartNames);
